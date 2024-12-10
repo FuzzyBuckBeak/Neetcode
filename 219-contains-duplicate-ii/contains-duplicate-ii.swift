42348fd20e5769d2cce59788e6d9cea274ce6e72
Apoorva Shettigar <shettigarapoorva@gmail.com>
2024-12-09 22:03:26 -0800

    Time: 20 ms (66.33%) | Memory: 20.3 MB (55.97%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var hash: [Int: Int] = [:]
+        for index in 0 ..< nums.count {
+            if let startIndex = hash[nums[index]] {
+                if abs(startIndex-index) <= k {
+                    return true
+                } else {
+                    hash[nums[index]] = index
+                }
+            }
+        
+            if hash[nums[index]] == nil {
+                hash[nums[index]] = index
+            }
+        
+        }
+    
+        return false
+    }
+}
